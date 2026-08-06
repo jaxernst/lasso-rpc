ce479fd483e4a7bfe92b478c8cd45b8a4d6ecda1
Jackson Ernst - jaxer.eth <51183683+jaxernst@users.noreply.github.com>
2026-08-05 22:45:07 -0700

    Remove accidental local artifacts from release PR

--- lib/lasso/core/request/request_options.ex
+++ lib/lasso/core/request/request_options.ex
@@ -9,7 +9,6 @@ defmodule Lasso.RPC.RequestOptions do
   alias Lasso.Config.MethodConstraints
 
   @type strategy :: :fastest | :priority | :load_balanced | :latency_weighted
-
   @type transport :: :http | :ws | :both | nil
 
   @default_profile Lasso.Config.ProfileValidator.default_profile()
